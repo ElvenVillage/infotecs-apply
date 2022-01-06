bf200df72cf326c2ced1d16a53c080cd2dd8dc94
ElvenVillage <dvolkovdd@gmail.com>
2022-01-06 12:19:38 +0300

    цвета, колонки

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const head = document.getElementById('head');
 const root = document.getElementById('root');
 const editor = document.getElementById('editor');
+const toggler = document.getElementById('toggler');
+
+/*
+Создаем ссылки на DOM-элементы основых частей страницы, 
+а также кнопок "Вперед" и "Назад"
+*/
 
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
-const properties = {
+const properties = { 
+    /*
+    Словарь отображаемых названий полей
+    */
     'firstName': 'Имя',
     'lastName': 'Фамилия',
     'about': 'Описание',
     'eyeColor': 'Цвет глаз'
 }
 
-let visibleProperties = ['firstName', 'lastName', 'about', 'eyeColor'];
-let sortedProperty = null;
-let editableElement = null;
+//Список полей, отображаемых в данный момент в таблице
 
+let visibleProperties = ['firstName', 'lastName', 'about', 'eyeColor'];
+
+//При инициализации нет ни сортировки, ни редактирования
+let sortedProperty = null;
+let editableElementId = null;
+
+//Начинаем отображение с первой страницы
 let page = 0;
 
+
 function getProperty(element, property) {
+    /*
+    Геттер, позволяющий использовать структуру как плоскую. 
+    */
     if (property == 'firstName' || property == 'lastName') {
         return element['name'][property];
     } 
@@ -26,6 +44,9 @@ function getProperty(element, property) {
 }
 
 function setProperty(element, property, value) {
+    /*
+    Сеттер, позволяющий использовать структуру как плоскую.
+    */
     if (property == 'firstName' || property == 'lastName') {
         element['name'][property] = value;
     } else {
@@ -33,11 +54,13 @@ function setProperty(element, property, value) {
     }
 }
 
-
+//Обработчик клика по заголовку колонки
 function sort(property) {
+    //Перерисуем заголовок, отобразив, по какому полю сортируем
     sortedProperty = property;
     renderHeader();
 
+    //Сортируем по выбранному полю
     const sortedData = data.slice();
     sortedData.sort((a, b) => {
         const propertyA = getProperty(a, property);
@@ -47,32 +70,60 @@ function sort(property) {
         if (propertyA == propertyB) return 0;
         return -1;
     });
+    //Перерисуем тело таблицы
     renderTable(sortedData);
 }
 
+//Вызывается при клике по строке таблицы
+function edit(elem) {
+    //Перенесем в поля ввода значения 
+    Object.keys(properties).forEach((property) => {
+        document.getElementById(property).value = getProperty(elem, property);
+    });
+    editableElementId = elem.id;
+}
+
+//Создание заголовка таблицы
 function renderHeader() {
+    //Очистим прошлый возможный заголовок
     head.innerHTML = '';
     
     head.append(...visibleProperties.map((property) => {
         const header = document.createElement('th');
         let text = properties[property];
+
+        //Отметим отсортированный столбец
         if (property === sortedProperty) {
             text += '⬇';
         } 
+
         header.innerText = text;
-        header.onclick = () => sort(property);
+        header.onclick = () => {
+            page = 0;
+            sort(property);
+        }
         return header;
     }));
 }
 
+//Создание тела таблицы
 function renderTable(data) {
+    //Очистим прошлую возможную таблицу
     root.innerHTML = '';
+
+    //Берем текущую страницу по индексу page
     root.append(...data.slice(page*10, (page+1)*10).map((elem) => {
         const row = document.createElement('tr');
         row.onclick = () => edit(elem);
         row.append(...visibleProperties.map((property) => {
             const cell = document.createElement('td');
-            cell.innerText = getProperty(elem, property);
+            const value = getProperty(elem, property);
+            //Красим колонку с цветом 
+            if (property === 'eyeColor') {
+                cell.style = `background-color:${value}`;
+            } else {
+                cell.innerText = value;
+            }
             cell.className = property;
             return cell;
         }));
@@ -80,6 +131,7 @@ function renderTable(data) {
     }));
 }
 
+//Вызывается один раз при инициализации
 function renderEditor() {
     const inputs = Object.keys(properties).flatMap((property) => {
         const label = document.createElement('label');
@@ -98,7 +150,8 @@ function renderEditor() {
     });
     const submit = document.createElement('input'); 
     submit.type = 'submit';
-    submit.innerText = 'Редактировать';
+    submit.id = 'submit';
+    submit.value = 'Редактировать';
     submit.onclick = (e) => {
         e.preventDefault();
         submitChanges();
@@ -106,22 +159,52 @@ function renderEditor() {
     editor.append(...inputs, submit);
 }
 
-function edit(elem) {
-    Object.keys(properties).forEach((property) => {
-        document.getElementById(property).value = getProperty(elem, property);
-    });
-    editableElement = elem;
+//Вызывается один раз при инициализации
+function renderToggler() {
+    toggler.append(...Object.keys(properties).map((property) => {
+        const div = document.createElement('div');
+        div.className = 'toggleCheckbox';
+
+        const label = document.createElement('label');
+        const id = `${property}Label`;
+        label.htmlFor = id;
+        label.innerText = properties[property];
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox'; checkbox.id = id; 
+        checkbox.checked = true; //Изначально все поля отображаются
+        checkbox.className = 'checkbox';
+
+        checkbox.onchange = () => {
+            //Перерисуем таблицу с новыми visibleProperties
+            visibleProperties = Object.keys(properties).filter((prop) => {
+                return document.getElementById(`${prop}Label`).checked;
+            });
+            renderSortedTable(data);
+        };
+
+        div.append(label, checkbox);
+        return div;
+    }));
 }
 
+//Обработчик submit формы
 function submitChanges() {
+    //Находим элемент с нужным id в цикле
     for (var i = 0; i < data.length; i++) {
-        if (data[i].id === editableElement['id']) {
-            Object.keys(properties).forEach((property) => setProperty(data[i], property, document.getElementById(property).value));
+        if (data[i].id === editableElementId) {
+            //С помощью сеттера перенесем значения из формы
+            Object.keys(properties).forEach((property) => 
+                setProperty(data[i], property, document.getElementById(property).value));
             renderSortedTable(data);
         }
     }
 }
 
+/*
+Перерисовывает таблицу с учетом сортировки после изменения элемента и/или
+списка отображаемых столбцов
+*/
 function renderSortedTable(data) {
     if (sortedProperty != '') {
         sort(sortedProperty);
@@ -144,5 +227,6 @@ window.onload = () => {
 
     renderHeader();
     renderTable(data);
+    renderToggler();
     renderEditor();
 };
